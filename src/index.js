0a7f738fefcb0a9fe4db4e4e8a8bf888760fc852
Cersei <circegrand@gmail.com>
2018-12-03 14:56:58 +0100

    Init graphQL file

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const httpLink = new HttpLink({ uri: 'https://api.graph.cool/simple/v1/cjp2pniu98sw30122z5bavdwc' });
+const client = new ApolloClient({
+    link: httpLink,
+    cache: new InMemoryCache()
+});
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
+ReactDOM.render(
+    <ApolloProvider client={client}>
+        <h1>P.O.C</h1>
+        <p></p>
+    </ApolloProvider>
+    , document.getElementById('root'));
+
 serviceWorker.unregister();
