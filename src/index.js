283bc369d80403d00c174a1fa4d63f818f786271
Cersei <circegrand@gmail.com>
2018-12-04 10:29:42 +0100

    Show allEpisodes

--- src/index.js
+++ src/index.js
@@ -1,30 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 import { ApolloProvider, Query } from 'react-apollo';
-// import { ApolloClient } from 'apollo-client';
 import ApolloBoost from "apollo-boost";
 import gql from "graphql-tag";
-// import { HttpLink } from 'apollo-link-http';
-// import { InMemoryCache } from 'apollo-cache-inmemory';
 
-
-// const httpLink = new HttpLink({ uri: 'https://api.graph.cool/simple/v1/cjp2pniu98sw30122z5bavdwc' });
-// const client = new ApolloClient({
-//     link: httpLink,
-//     cache: new InMemoryCache()
-// });
-
-const clientBoost = new ApolloBoost({
+const client = new ApolloBoost({
     uri: "https://api.graph.cool/simple/v1/cjp2pniu98sw30122z5bavdwc"
     // uri: "https://w5xlvm3vzz.lp.gql.zone/graphql"
 });
 
-const GET_EPISODE = gql`    
+const GET_EPISODE = gql`
     {
         allEpisodes {
             id
+            title
             summary
         }
     }
@@ -37,10 +29,10 @@ const Episodes = ({ onEpisodeSelected }) => (
             if (error) return `Error! ${error.message}`;
 
             return (
-                <select name="dog" onChange={onEpisodeSelected}>
-                    {data.episodes.map(dog => (
-                        <option key={dog.id} value={dog.summary}>
-                            {dog.title}
+                <select name="episode" onChange={onEpisodeSelected}>
+                    {data.allEpisodes.map(episode => (
+                        <option key={episode.id} value={episode.title}>
+                            {episode.title}
                         </option>
                     ))}
                 </select>
@@ -49,19 +41,16 @@ const Episodes = ({ onEpisodeSelected }) => (
     </Query>
 );
 
-clientBoost
+client
 .query({
     query: GET_EPISODE
 })
 .then(result => console.log(result));
 
 ReactDOM.render(
-    <ApolloProvider client={clientBoost}>
-        <h1>P.O.C</h1>
-        <p></p>
+    <ApolloProvider client={client}>
+        <Episodes />
     </ApolloProvider>
     , document.getElementById('root'));
 
 serviceWorker.unregister();
-
-
