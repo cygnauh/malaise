0ef6428fd64ee702e201c9c25d6c8fd3978ae092
Cersei <circegrand@gmail.com>
2018-12-04 16:10:19 +0100

    WIP - error

--- src/components/interactions/started.js
+++ src/components/interactions/started.js
@@ -4,7 +4,7 @@ class Started extends React.Component {
     render() {
         return (
             <section className="form form__name">
-
+                <p>coucou test started</p>
             </section>
         )
     }
--- src/index.js
+++ src/index.js
@@ -5,8 +5,6 @@ import * as serviceWorker from './serviceWorker';
 import { ApolloProvider, Query } from 'react-apollo';
 import ApolloBoost from "apollo-boost";
 import {App, GET_FIRST_EPISODE} from './App';
-import Header from './components/layout/Header';
-import Timeline from './components/layout/Timeline';
 
 const client = new ApolloBoost({
     uri: "https://api.graph.cool/simple/v1/cjp2pniu98sw30122z5bavdwc"
@@ -20,9 +18,7 @@ client
 
 ReactDOM.render(
     <ApolloProvider client={client}>
-        <Header/>
         <App />
-        <Timeline/>
     </ApolloProvider>
     , document.getElementById('root'));
 
