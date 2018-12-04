cf330392c47fe935bccb0ed95c28450200624201
Christine Yini Huang <cygnauh@gmail.com>
2018-12-04 23:50:46 +0100

    test howler sound

---
+++ src/components/Sound.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { Howl, Howler } from 'howler';
+class Sound extends React.Component {
+
+
+    constructor(props) {
+        super(props);
+        this.sourceMp3Web = 'https://circegrand.fr/etude/gobelins/malaise/media/sounds/party-ambience-1-no-copyright-sound-effect.mp3';
+        this.source = this.props.source;
+        this.stream = new Howl({
+            src: [this.source],
+            ext: ['mp3'],
+            autoplay: true,
+            html5: true
+        });
+        this.stream.play()
+    }
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <h1>Malaise.</h1>
+                </header>
+            </div>
+        );
+    }
+}
+
+export default Sound;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+// import App from './App';
 import './index.css';
+import SoundTest from './components/Sound'
 import * as serviceWorker from './serviceWorker';
+
+//Apollo, handle Data
 import { ApolloProvider, Query } from 'react-apollo';
 import ApolloBoost from "apollo-boost";
 import gql from "graphql-tag";
+
+//Howler, handle sound
+// import ReactHowler from 'react-howler'
+// import {Howl} from "howler";
 
 const client = new ApolloBoost({
     uri: "https://api.graph.cool/simple/v1/cjp2pniu98sw30122z5bavdwc"
@@ -21,6 +28,30 @@ const GET_EPISODE = gql`
         }
     }
 `;
+
+const GET_EPISODE_SOUND_URL = gql`
+    {
+        Episode(
+            id:"cjp8r6rp2p9eg0183cgcd45qz"
+        ) {
+            sounds {
+                url
+            }
+        }
+    }
+`;
+
+
+let test = null;
+
+client
+.query({
+    query: GET_EPISODE_SOUND_URL
+})
+.then(result => function(){test = result;console.log(test)});
+
+
+
 
 const Episodes = ({ onEpisodeSelected }) => (
     <Query query={GET_EPISODE}>
@@ -41,15 +72,10 @@ const Episodes = ({ onEpisodeSelected }) => (
     </Query>
 );
 
-client
-.query({
-    query: GET_EPISODE
-})
-.then(result => console.log(result));
-
 ReactDOM.render(
     <ApolloProvider client={client}>
         <Episodes />
+        <SoundTest source={'https://circegrand.fr/etude/gobelins/malaise/media/sounds/party-ambience-1-no-copyright-sound-effect.mp3'}/>
     </ApolloProvider>
     , document.getElementById('root'));
 
