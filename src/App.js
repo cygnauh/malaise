976bd40d5bd111243c00cf3a71cd5cd5c46a7c5d
Christine Yini Huang <cygnauh@gmail.com>
2018-12-05 17:35:26 +0100

    migrate index code to app

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
 import './App.css';
 import React, { Component } from 'react';
 import Started from "./components/interactions/started";
-import Doorbell from "./components/interactions/doorbell";
+import SoundTest from './components/Sound'
+import Header from './components/layout/Header';
+
+import { ApolloProvider, Query } from 'react-apollo';
+import ApolloBoost from "apollo-boost";
+import Timeline from './components/layout/Timeline';
+
+
+
+// import Doorbell from "./components/interactions/doorbell";
 
 class App extends Component {
     constructor(){
         super();
         this.state = {render:''}
+        this.client = new ApolloBoost({
+            uri: "https://api.graph.cool/simple/v1/cjp2pniu98sw30122z5bavdwc"
+        });
     }
     render() {
         return (
             <main>
-                <Started />
+                <div>
+                    <ApolloProvider client={this.client}>
+                        <Header/>
+                        <Started />
+                        <SoundTest
+                            source={'https://circegrand.fr/etude/gobelins/malaise/media/sounds/party-ambience-1-no-copyright-sound-effect.mp3'}
+                        />
+                    </ApolloProvider>
+                </div>
             </main>
         )
     }
--- src/index.js
+++ src/index.js
@@ -1,29 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from "./App";
 import './index.css';
-import SoundTest from './components/Sound'
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-//Apollo, handle Data
-import { ApolloProvider, Query } from 'react-apollo';
-import ApolloBoost from "apollo-boost";
-import Header from './components/layout/Header';
-import Timeline from './components/layout/Timeline';
+ReactDOM.render(<App />, document.getElementById('root'));
 
-const client = new ApolloBoost({
-    uri: "https://api.graph.cool/simple/v1/cjp2pniu98sw30122z5bavdwc"
-});
-
-ReactDOM.render(
-    <ApolloProvider client={client}>
-        <Header/>
-        {/*<App/>*/}
-        {/*<Timeline/>*/}
-        <SoundTest
-            source={'https://circegrand.fr/etude/gobelins/malaise/media/sounds/party-ambience-1-no-copyright-sound-effect.mp3'}
-        />
-    </ApolloProvider>
-    , document.getElementById('root'));
-
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
