7a5eb6e14ae66b75567d908b46a58588949ea4c4
Christine Yini Huang <cygnauh@gmail.com>
2018-12-06 15:14:47 +0100

    test anim with lottie

--- src/components/interactions/Doorbell.js
+++ src/components/interactions/Doorbell.js
@@ -1,40 +1,77 @@
 import "./interactions.css";
 import React from 'react';
-import ReactBodymovin from 'react-bodymovin'
-import animation from '../../assets/animation/test'
+// import ReactBodymovin from 'react-bodymovin'
+import Lottie from 'react-lottie';
+import animationData from '../../assets/animation/fusion'
+import { Howl, Howler } from 'howler';
 
 class Doorbell extends React.Component {
     constructor(props) {
         super(props);
+        this.JsonSessionStorage = JSON.parse(window.sessionStorage.getItem('characters'))
         this.state = {
-            hostName: window.sessionStorage.getItem('host')
+            hostName: this.JsonSessionStorage.name,
+            openDoor:false,
+            blockBell:false
         };
-        this.bodymovinOptions = {
+        this.defaultOptions = {
             loop: true,
             autoplay: true,
-            prerender: true,
-            animationData: animation
-        }
+            animationData: animationData,
+            rendererSettings: {
+                preserveAspectRatio: 'xMidYMid slice'
+            }
+        };
+        this.sourceDoorbellWeb = 'https://circegrand.fr/etude/gobelins/malaise/media/sounds/doorbell.mp3';
+            this.source = this.sourceDoorbellWeb;
+        this.stream = new Howl({
+            src: [this.source],
+            ext: ['mp3'],
+        html5: true
+    });
+        console.log(animationData)
     }
-    getHostName(){
-        var label = document.querySelectorAll('.Doorbell__name');
-        console.log(label);
+    handleBellClick = (evt) => {
+        evt.preventDefault();
+        console.log('doorbell');
+        this.stream.play();
+        if(!this.state.blockBell){
+            setTimeout(
+                () => {
+                    this.setState({
+                        openDoor: true
+                    });
+                }, 2000
+            );
+        }
+
+        this.setState({
+            blockBell : true
+        })
+
+
     }
     render() {
         return (
-            <div>
-                {/*<div className="Interaction">*/}
-                    {/*<div className="Doorbell">*/}
-                        {/*<div className="Doorbell__object">*/}
-                            {/*<button className="Doorbell__button"/>*/}
-                            {/*<label className="Doorbell__name">{this.state.hostName}</label>*/}
-                        {/*</div>*/}
-                    {/*</div>*/}
-                {/*</div>*/}
-                <div>
-                    <ReactBodymovin options={this.bodymovinOptions} />
+        <div className="Interaction">
+            {!this.state.openDoor
+            ?
+            <div className="Doorbell">
+                <div className="Doorbell__object">
+                    <button className="Doorbell__button" onClick={this.handleBellClick}/>
+                    <label className="Doorbell__name">{this.state.hostName}</label>
                 </div>
             </div>
+            :
+            <div>
+                <Lottie options={this.defaultOptions}
+                        height={400}
+                        width={400}
+                        isStopped={this.state.isStopped}
+                        isPaused={this.state.isPaused}/>
+            </div>
+            }
+        </div>
         );
     }
 }
--- src/components/interactions/doorbell.js
+++ src/components/interactions/doorbell.js
@@ -1,7 +1,8 @@
 import "./interactions.css";
 import React from 'react';
-import ReactBodymovin from 'react-bodymovin'
-import animation from '../../assets/animation/test'
+// import ReactBodymovin from 'react-bodymovin'
+import Lottie from 'react-lottie';
+import animationData from '../../assets/animation/fusion'
 import { Howl, Howler } from 'howler';
 
 class Doorbell extends React.Component {
@@ -9,14 +10,18 @@ class Doorbell extends React.Component {
         super(props);
         this.JsonSessionStorage = JSON.parse(window.sessionStorage.getItem('characters'))
         this.state = {
-            hostName: this.JsonSessionStorage.name
+            hostName: this.JsonSessionStorage.name,
+            openDoor:false,
+            blockBell:false
         };
-        this.bodymovinOptions = {
+        this.defaultOptions = {
             loop: true,
             autoplay: true,
-            prerender: true,
-            animationData: animation
-        }
+            animationData: animationData,
+            rendererSettings: {
+                preserveAspectRatio: 'xMidYMid slice'
+            }
+        };
         this.sourceDoorbellWeb = 'https://circegrand.fr/etude/gobelins/malaise/media/sounds/doorbell.mp3';
             this.source = this.sourceDoorbellWeb;
         this.stream = new Howl({
@@ -24,27 +29,49 @@ class Doorbell extends React.Component {
             ext: ['mp3'],
         html5: true
     });
+        console.log(animationData)
     }
     handleBellClick = (evt) => {
         evt.preventDefault();
         console.log('doorbell');
         this.stream.play();
+        if(!this.state.blockBell){
+            setTimeout(
+                () => {
+                    this.setState({
+                        openDoor: true
+                    });
+                }, 2000
+            );
+        }
+
+        this.setState({
+            blockBell : true
+        })
+
+
     }
     render() {
         return (
-            <div>
-                <div className="Interaction">
-                    <div className="Doorbell">
-                        <div className="Doorbell__object">
-                            <button className="Doorbell__button" onClick={this.handleBellClick}/>
-                            <label className="Doorbell__name">{this.state.hostName}</label>
-                        </div>
-                    </div>
+        <div className="Interaction">
+            {!this.state.openDoor
+            ?
+            <div className="Doorbell">
+                <div className="Doorbell__object">
+                    <button className="Doorbell__button" onClick={this.handleBellClick}/>
+                    <label className="Doorbell__name">{this.state.hostName}</label>
                 </div>
-                {/*<div>*/}
-                    {/*<ReactBodymovin options={this.bodymovinOptions} />*/}
-                {/*</div>*/}
             </div>
+            :
+            <div>
+                <Lottie options={this.defaultOptions}
+                        height={400}
+                        width={400}
+                        isStopped={this.state.isStopped}
+                        isPaused={this.state.isPaused}/>
+            </div>
+            }
+        </div>
         );
     }
 }
