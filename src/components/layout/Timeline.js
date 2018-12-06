5247175da0ae65d965e6bad13117065bcc1ece1b
Cersei <circegrand@gmail.com>
2018-12-06 18:38:27 +0100

    WIP - Timeline

--- src/components/layout/Timeline.js
+++ src/components/layout/Timeline.js
@@ -2,10 +2,17 @@ import "./Timeline.css"
 import React from 'react';
 
 class Timeline extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
     render() {
         return (
             <div className="Timeline">
-                <div className="Timeline__progress-bar"></div>
+                <div className="Timeline__progress-bar">
+                    <div className="Timeline__progress-bar--default" style="30%"></div>
+                    <div className="Timeline__progress-bar--progression"></div>
+                </div>
             </div>
         )
     }
