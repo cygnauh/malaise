5ecf0986423f2dde9f9049ac034aaa7cee7cd0cd
Cersei <circegrand@gmail.com>
2019-01-09 00:12:28 +0100

    WIP - Bell animation

--- src/components/pages/Dictionnary.js
+++ src/components/pages/Dictionnary.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import '../../assets/styles/dictionnary.css';
 
 class Dictionnary extends Component {
 
@@ -6,10 +7,25 @@ class Dictionnary extends Component {
         super(props);
     }
 
+    handleClick(e) {
+        e.preventDefault();
+        console.log('Btn clicked');
+    }
+
     render() {
         return (
             <div className="Dictionnary">
                 Dictionnaire
+                <div className="test">
+                    <div className="bell">
+                        <label>ce soir tu es invité(e) chez ta pote qui fait une soirée posée</label>
+                        <div className="bell__box">
+                            <input className="bell__input" type="text" placeholder="son prénom" />
+                            <button className="btn btn__input" onClick={this.handleClick}>OK</button>
+                        </div>
+                    </div>
+                </div>
+
             </div>
         )
     }
