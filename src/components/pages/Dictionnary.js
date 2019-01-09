214df4f830e302b381f6ee4d3239b00a0edee97f
Cersei <circegrand@gmail.com>
2019-01-09 14:57:34 +0100

    WIP - Bell animation

--- src/components/pages/Dictionnary.js
+++ src/components/pages/Dictionnary.js
@@ -5,11 +5,19 @@ class Dictionnary extends Component {
 
     constructor(props) {
         super(props);
+        this.state = {
+            render:'',
+            activeBell: false
+        };
+        this.handleClick = this.handleClick.bind(this);
     }
 
-    handleClick(e) {
-        e.preventDefault();
+    handleClick() {
         console.log('Btn clicked');
+        console.log(this);
+        this.setState({
+            activeBell:true
+        });
     }
 
     render() {
@@ -17,11 +25,13 @@ class Dictionnary extends Component {
             <div className="Dictionnary">
                 Dictionnaire
                 <div className="test">
-                    <div className="bell">
+                    <div className={this.state.activeBell ? 'bell animate-bell': 'bell'}>
                         <label>ce soir tu es invité(e) chez ta pote qui fait une soirée posée</label>
                         <div className="bell__box">
-                            <input className="bell__input" type="text" placeholder="son prénom" />
-                            <button className="btn btn__input" onClick={this.handleClick}>OK</button>
+                            <div className="bell__input-border">
+                                <input className="bell__input" type="text" placeholder="son prénom" />
+                            </div>
+                            <button className="btn btn__input" onClick={this.handleClick}><span>OK</span></button>
                         </div>
                     </div>
                 </div>
