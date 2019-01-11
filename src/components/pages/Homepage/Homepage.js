99e2c903d0f716005be9ba782b436eabb2b2006f
Cersei <circegrand@gmail.com>
2019-01-11 16:13:48 +0100

    Eject Webpack + add color + add typo + begin HP

--- src/components/pages/Homepage/Homepage.js
+++ src/components/pages/Homepage/Homepage.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import "../../../assets/styles/index.scss";
 import "./style.scss";
 
 class Homepage extends Component {
@@ -32,11 +31,74 @@ class Homepage extends Component {
                 <div className="Home__container">
                     <div className="Home__center">
                         <div className="Home__content">
-                            <h1 className="Home__title">malaise.</h1>
+                            <img className="Home__logo" src={require('../../../assets/icons/logo__white.png')} />
                             <p className="Home__description">Une websérie immersive sonore qui vous plonge au coeur d'histoires et d'anecdotes du quotidien.</p>
                             <div className="Home__sound">
-                                <img src={require('../../../assets/icons/headphones.svg')} />
-                                <p>Activez le son et utilisez un casque ou des écouteurs pour une meilleure immersion.</p>
+                                <div className="Home__sound-rotation">
+                                    <div className="Home__sound-letters">
+                                        <div className="char1">b</div>
+                                        <div className="char2">r</div>
+                                        <div className="char3">a</div>
+                                        <div className="char4">n</div>
+                                        <div className="char5">c</div>
+                                        <div className="char6">h</div>
+                                        <div className="char7">e</div>
+                                        <div className="char8"></div>
+                                        <div className="char9">t</div>
+                                        <div className="char10">o</div>
+                                        <div className="char11">n</div>
+                                        <div className="char12"></div>
+                                        <div className="char13">c</div>
+                                        <div className="char14">a</div>
+                                        <div className="char15">s</div>
+                                        <div className="char16">q</div>
+                                        <div className="char17">u</div>
+                                        <div className="char18">e</div>
+                                        <div className="char19"></div>
+                                        <div className="char20"></div>
+                                        <div className="char21">b</div>
+                                        <div className="char22">r</div>
+                                        <div className="char23">a</div>
+                                        <div className="char24">n</div>
+                                        <div className="char25">c</div>
+                                        <div className="char26">h</div>
+                                        <div className="char27">e</div>
+                                        <div className="char28"></div>
+                                        <div className="char29">t</div>
+                                        <div className="char30">o</div>
+                                        <div className="char31">n</div>
+                                        <div className="char32"></div>
+                                        <div className="char33">c</div>
+                                        <div className="char34">a</div>
+                                        <div className="char35">s</div>
+                                        <div className="char36">q</div>
+                                        <div className="char37">u</div>
+                                        <div className="char38">e</div>
+                                        <div className="char39"></div>
+                                        <div className="char40"></div>
+                                        <div className="char41">b</div>
+                                        <div className="char42">r</div>
+                                        <div className="char43">a</div>
+                                        <div className="char44">n</div>
+                                        <div className="char45">c</div>
+                                        <div className="char46">h</div>
+                                        <div className="char47">e</div>
+                                        <div className="char48"></div>
+                                        <div className="char49">t</div>
+                                        <div className="char50">o</div>
+                                        <div className="char51">n</div>
+                                        <div className="char52"></div>
+                                        <div className="char53">c</div>
+                                        <div className="char54">a</div>
+                                        <div className="char55">s</div>
+                                        <div className="char56">q</div>
+                                        <div className="char57">u</div>
+                                        <div className="char58">e</div>
+                                        <div className="char59"></div>
+                                        <div className="char60"></div>
+                                    </div>
+                                </div>
+                                <img className="Home__sound-icon" src={require('../../../assets/icons/headphones.png')} />
                             </div>
                             <button className="Home__start" onClick={this.next}>démarrer l'expérience</button>
                         </div>
