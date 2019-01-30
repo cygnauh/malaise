3f55cd131c42e6a9629067b7b73af3d49b02f1ef
Christine Yini Huang <cygnauh@gmail.com>
2019-01-30 01:29:18 +0100

    WIP - interactions : display interation settimeout sound duration

--- src/store/SoundProvider.js
+++ src/store/SoundProvider.js
@@ -45,7 +45,7 @@ class SoundProvider extends Component {
                 }
             }, 0);
             let sound = new Howl({
-                src: [this.state.episodeSoundtrack],
+                src: [Sound],
                 // sprite:{
                 //     regles: [35250, 5930],
                 //     lancement_jeu: [13040, 19840],
@@ -138,6 +138,11 @@ class SoundProvider extends Component {
         playInteractionSound:(value) =>{
             // this.state.episodeSounds
             this.state.episodeSounds.play(value);
+            return this.state.episodeSounds._sprite[value][1]
+            // this.state.episodeSounds.on('end', () => {
+            //     console.log("this is over");
+            //     return true
+            // });
         }
     };
 
