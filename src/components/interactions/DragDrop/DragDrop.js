e7fbec498da5ef3b6bdd5c5e6e1a08c451096cec
Cersei <circegrand@gmail.com>
2019-01-30 01:18:43 +0100

    WIP - add anim on stop draggable

--- src/components/interactions/DragDrop/DragDrop.js
+++ src/components/interactions/DragDrop/DragDrop.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './style.scss';
 import Draggable from 'react-draggable';
+import $ from 'jquery';
 
 class DragDrop extends Component {
 
@@ -8,16 +9,24 @@ class DragDrop extends Component {
         super(props);
     }
 
+    handleStop = () => {
+        console.log('stop handle');
+        $('.Drag-drop__dragEnd').addClass('Drag-drop__dragEnd--full');
+        $('.Drag-drop__draggable').addClass('Drag-drop__draggable--dragged');
+    }
+
     render() {
         return (
             <div className="Drag-drop">
                 <div className="Drag-drop__dragEnd"></div>
-                <div className="Drag-drop__dragStart">
+                <div className="Drag-drop__dragBottom">
+                    <div className="Drag-drop__dragStart"></div>
                     <Draggable
                         axis="y"
                         handle=".handle"
                         bounds={{top: -150, bottom: 0}}
                         defaultPosition={{x: 0, y: 0}}
+                        defaultClassName="Drag-drop__dragElement"
                         position={null}
                         scale={1}
                         onStart={this.handleStart}
