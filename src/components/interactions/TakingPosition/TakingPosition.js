a570b5fc568fc40f18ad940aa75c90f9c61c59bf
Cersei <circegrand@gmail.com>
2019-02-04 13:55:18 +0100

    WIP: taking position

--- src/components/interactions/TakingPosition/TakingPosition.js
+++ src/components/interactions/TakingPosition/TakingPosition.js
@@ -6,7 +6,10 @@ class TakingPosition extends Component {
 
     constructor(props) {
         super(props);
-        this.state = { x: 0 };
+        this.state = {
+            x: 0,
+            defaultWidth: 0
+        };
 
         this.part1 = React.createRef();
         this.part2 = React.createRef();
@@ -19,25 +22,26 @@ class TakingPosition extends Component {
         const p1 = this.part1.current;
         const p2 = this.part2.current;
 
-        this.setState({ x: e.screenX });
-        console.log('x:' + this.state.x);
 
-        var p1Width = 100 - this.state.x/10;
-        var p2Width = this.state.x/10;
+        let $currentTarget = $(e.currentTarget);
 
-        p2.style.width = p2Width + '%';
-        p1.style.width = p1Width + '%';
+        this.setState({
+            x: e.pageX - $currentTarget.offset().left
+        });
 
-        if ( p1Width > 50 && !p1.classList.contains("TakingPosition__winner") ) {
-            console.log('si part 1 > 50');
+        let x = this.state.x;
+
+        p1.style.width = 100 - x / 10 + '%';
+        p2.style.width = x / 10 + '%';
+
+        if (x < e.currentTarget.offsetWidth / 2) {
             p1.className += " TakingPosition__winner";
             p2.classList.remove("TakingPosition__winner");
-        } else if (p2Width > 50 && !p2.classList.contains("TakingPosition__winner")) {
-            console.log('si part 2 > 50');
+
+        } else {
             p2.className += " TakingPosition__winner";
             p1.classList.remove("TakingPosition__winner");
         }
-
     }
 
     render() {
