09c34b1ad1f969ae43569c81383af2feb365e86b
Cersei <circegrand@gmail.com>
2019-02-04 14:53:51 +0100

    WIP: taking position mouse move

--- src/components/interactions/TakingPosition/TakingPosition.js
+++ src/components/interactions/TakingPosition/TakingPosition.js
@@ -33,15 +33,23 @@ class TakingPosition extends Component {
 
         p1.style.width = 100 - x / 10 + '%';
         p2.style.width = x / 10 + '%';
+        
+        var inverseX = (e.currentTarget.offsetWidth / 2) - x;
+        p1.querySelector('.TakingPosition__name').style.transform = `scale(${1 + inverseX/500})`;
+        p2.querySelector('.TakingPosition__name').style.transform = `scale(${1 - inverseX/500})`;
 
         if (x < e.currentTarget.offsetWidth / 2) {
-            p1.className += " TakingPosition__winner";
-            p2.classList.remove("TakingPosition__winner");
-
-        } else {
-            p2.className += " TakingPosition__winner";
-            p1.classList.remove("TakingPosition__winner");
+            if(!p1.classList.contains("TakingPosition__winner")) {
+                p1.className += " TakingPosition__winner";
+                p2.classList.remove("TakingPosition__winner");
+            }
+        } else if(x > e.currentTarget.offsetWidth / 2) {
+            if(!p2.classList.contains("TakingPosition__winner")) {
+                p2.className += " TakingPosition__winner";
+                p1.classList.remove("TakingPosition__winner");
+            }
         }
+
     }
 
     render() {
