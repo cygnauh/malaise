df8f4009d1075ec81c3fbfce3c7ab918bafa7664
Cersei <circegrand@gmail.com>
2019-02-05 14:45:05 +0100

    About integration DONE

--- src/components/pages/About/About.js
+++ src/components/pages/About/About.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './style.scss';
 import { Link } from 'react-router-dom';
+import GobelinsLogo from '../../../assets/img/logo-gobelins.png';
 
 
 class About extends Component {
@@ -18,16 +19,17 @@ class About extends Component {
                         <p className="About__description__content">Que ce soit avec vos amis, vos collègues, votre famille, en soirée ou dans la rue ? Rappelez vous, quand vous vouliez parler d’un sujet mais que vous cherchiez comment l’aborder, ou que vous avez blesser involontairement votre interlocuteur à cause d’une mal formulation. Malaise. est là pour vous informer et vous faire prendre conscience de l’importance des mots et de leurs significations dans une discussion.Pour faire simple, Malaise. est une plateforme d’expériences narratives qui se découpent en différents épisodes tournant autour de thématiques variés. L’utilisateur est amené dans chacun de ses épisodes à participer, en interagissant avec l’histoire. </p>
                     </div>
                     <div className="About__team">
-                        <h2 className="team__title">la team</h2>
+                        <h2 className="team__title"><span>la team</span></h2>
                         <div className="team__presentation">
-                            <div className="team__description">Lorem ipsum dolor sit amet andef mplode heur ikol pulio lorem doso. Mplode heur ikol puli. </div>
-                            <div className="team__logo"></div>
+                            <div className="team__description">Lorem ipsum dolor sit amet andef mplode heur ikol pulio lorem doso. Mplode heur ikol puli. Lorem ipsum dolor sit amet doleju szdeuj xzndzp defr. Juszdlo szndhue. Ppodehufre szcd husden mplide. Hydeja mplode ajiske nchu lsokd zaun. </div>
+                            <div className="team__logo"><img src={GobelinsLogo} /></div>
                         </div>
                         <div className="team__worker">
                             <div className="worker">
-                                <h3 className="worker__name">Noémie Eyoum</h3>
+                                <h3 className="worker__name">Noémie<br/>Eyoum</h3>
                                 <div className="worker__illustration"></div>
                                 <div className="worker__description">
+                                    Que ce soit avec vos amis, vos collègues, votre famille, en soirée ou dans la rue ? Rappelez vous, quand vous vouliez parler d’un sujet mais que vous cherchiez comment l’aborder
                                     <ul className="worker__socials-share">
                                         <li className="worker__social"><Link to="/">Link</Link></li>
                                         <li className="worker__social"><Link to="/">Port</Link></li>
@@ -36,9 +38,10 @@ class About extends Component {
                                 </div>
                             </div>
                             <div className="worker">
-                                <h3 className="worker__name">Christine Huang</h3>
+                                <h3 className="worker__name">Christine<br />Huang</h3>
                                 <div className="worker__illustration"></div>
                                 <div className="worker__description">
+                                    Que ce soit avec vos amis, vos collègues, votre famille, en soirée ou dans la rue ? Rappelez vous, quand vous vouliez parler d’un sujet mais que vous cherchiez comment l’aborder
                                     <ul className="worker__socials-share">
                                         <li className="worker__social"><Link to="/">Link</Link></li>
                                         <li className="worker__social"><Link to="/">Port</Link></li>
@@ -47,9 +50,10 @@ class About extends Component {
                                 </div>
                             </div>
                             <div className="worker">
-                                <h3 className="worker__name">Justine Lenouvel</h3>
+                                <h3 className="worker__name">Justine<br />Lenouvel</h3>
                                 <div className="worker__illustration"></div>
                                 <div className="worker__description">
+                                    Que ce soit avec vos amis, vos collègues, votre famille, en soirée ou dans la rue ? Rappelez vous, quand vous vouliez parler d’un sujet mais que vous cherchiez comment l’aborder
                                     <ul className="worker__socials-share">
                                         <li className="worker__social"><Link to="/">Link</Link></li>
                                         <li className="worker__social"><Link to="/">Port</Link></li>
@@ -58,9 +62,10 @@ class About extends Component {
                                 </div>
                             </div>
                             <div className="worker">
-                                <h3 className="worker__name">Circé Grand</h3>
+                                <h3 className="worker__name">Circé<br />Grand</h3>
                                 <div className="worker__illustration"></div>
                                 <div className="worker__description">
+                                    Que ce soit avec vos amis, vos collègues, votre famille, en soirée ou dans la rue ? Rappelez vous, quand vous vouliez parler d’un sujet mais que vous cherchiez comment l’aborder
                                     <ul className="worker__socials-share">
                                         <li className="worker__social"><Link to="/">Link</Link></li>
                                         <li className="worker__social"><Link to="/">Port</Link></li>
@@ -69,9 +74,10 @@ class About extends Component {
                                 </div>
                             </div>
                             <div className="worker">
-                                <h3 className="worker__name">Clara Vigourous</h3>
+                                <h3 className="worker__name">Clara<br />Vigourous</h3>
                                 <div className="worker__illustration"></div>
                                 <div className="worker__description">
+                                    Que ce soit avec vos amis, vos collègues, votre famille, en soirée ou dans la rue ? Rappelez vous, quand vous vouliez parler d’un sujet mais que vous cherchiez comment l’aborder
                                     <ul className="worker__socials-share">
                                         <li className="worker__social"><Link to="/">Link</Link></li>
                                         <li className="worker__social"><Link to="/">Port</Link></li>
@@ -81,7 +87,9 @@ class About extends Component {
                             </div>
                         </div>
                     </div>
-                    <div className="About__footer">aller aux épisodes</div>
+                    <div className="About__footer">
+                        <Link to="/">aller aux épisodes</Link>
+                    </div>
                 </div>
             </div>
         )
