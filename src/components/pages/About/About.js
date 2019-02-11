7b54763123233874822138b8ca1d86890ef490c4
Cersei <circegrand@gmail.com>
2019-02-11 22:01:20 +0100

    Replace h1 by SVG logo on about page

--- src/components/pages/About/About.js
+++ src/components/pages/About/About.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import './style.scss';
 import { Link } from 'react-router-dom';
 import GobelinsLogo from '../../../assets/img/logo-gobelins.png';
+import MalaiseLogo from '../../../assets/img/logos/logo-malaise.svg';
+import advice1 from '../../../assets/img/advice-1.png';
+import advice2 from '../../../assets/img/advice-2.png';
+import advice3 from '../../../assets/img/advice-3.png';
 import Heart from "../../SVG/Heart/Heart";
 
 
@@ -14,7 +18,9 @@ class About extends Component {
         return (
             <div className="About">
                 <div className="About__grid">
-                    <div className="About__grid--full About__title"><h1>malaise</h1></div>
+                    <div className="About__grid--full About__title">
+                        <img src={MalaiseLogo} alt="Logo de Malaise."/>
+                    </div>
                     <div className="About__grid--full About__description">
                         <p className="About__description__content">En 2019, à l’ère de l’info continue et de la mondialisation digitale, de plus en plus de controverses naissent autour des mots et de leur contexte. Black, homo, racisme, chinois, féministe, triso, genres... Peut-on parler de différence aujourd’hui sans offenser une partie de la population ? Beaucoup défendent la liberté d’expression sans limite, d’autres préfèreraient retirer certains termes de notre vocabulaire. <br /><br /><b>Les mots sont-ils si importants ?</b><br /><br />Nous connaissons tous cette sensation de malaise, vous savez : quand vous voulez parler d’un sujet sans savoir comment l’aborder de peur de gêner votre interlocuteur, que ce soit entre amis, entre collègues, en famille ou dans la rue...<br /><br />Dans le cadre privé, nous commettons aussi toutes et tous des maladresses entre amis ou au travail. En parlant, nous contribuons à alimenter des clichés, profondément ancrés dans la société, parfois sans même nous en rendre compte.<br /><br />Malaise est un podcast interactif qui nous plonge dans ce genre conversation anodine. Pendant l’écoute, l’utilisateur est amené à participer, en interagissant avec l’histoire. Chaque épisode permet de s’interroger sur un mot mal employé qui prolonge la discrimination.<br /><br />Pourquoi leur utilisation est-elle si répandue ? Comment le sens de certains mots a-t-il évolué et pris une connotation négative ? Leurs usages répétés nous ont amené à une banalisation.<br /><br />Nous pensons que nous interroger sur le sens des mots est une façon de questionner des schémas de pensées. Notre but est de ré-établir un dialogue entre ceux qui font des erreurs et ceux qui les subissent.</p>
                     </div>
@@ -23,7 +29,9 @@ class About extends Component {
                         <div className="advice__content">
                             <ul className="advice__podcastList">
                                 <li className="advice__podcastItem">
-                                    <div className="podcastItem__illustration"></div>
+                                    <div className="podcastItem__illustration">
+                                        <img src={advice1} alt="Illustration de Programme B"/>
+                                    </div>
                                     <div className="podcastItem__like">
                                         <Heart />
                                     </div>
@@ -31,7 +39,9 @@ class About extends Component {
                                     <p className="podcastItem__description">Comment éduquer à la tolérence.</p>
                                 </li>
                                 <li className="advice__podcastItem">
-                                    <div className="podcastItem__illustration"></div>
+                                    <div className="podcastItem__illustration">
+                                        <img src={advice2} alt="Illustration de Programme B"/>
+                                    </div>
                                     <div className="podcastItem__like">
                                         <Heart />
                                     </div>
@@ -39,7 +49,9 @@ class About extends Component {
                                     <p className="podcastItem__description">Les zinzins d'internet prennent-ils le pouvoir ?</p>
                                 </li>
                                 <li className="advice__podcastItem">
-                                    <div className="podcastItem__illustration"></div>
+                                    <div className="podcastItem__illustration">
+                                        <img src={advice3} alt="Illustration d'1 épisode et j'arrête"/>
+                                    </div>
                                     <div className="podcastItem__like">
                                         <Heart />
                                     </div>
@@ -53,7 +65,7 @@ class About extends Component {
                         <h2 className="team__title"><span>la team</span></h2>
                         <div className="team__presentation">
                             <div className="team__description">Malaise est né de la collaboration de cinq étudiantes des Gobelins. Chacune avec son vécu, ses anecdotes, ses moments de malaise, provoqués ou subis... Toutes différentes, elles ont tenu à faire un projet qui les réunissaient. Le projet a été ponctué par de multiples histoires, rires et débats. Tous ces échanges ont permis d’alimenter leurs réflexion sur le message qu’elles souhaitent faire passer : pour réinstaurer un dialogue avec l’autre, il ne faut pas utiliser un ton moralisateur car nous avons toutes et tous déjà blessé ou créé de la gêne. Pour les présentations détaillées, c’est juste en dessous ;)</div>
-                            <div className="team__logo"><img src={GobelinsLogo} /></div>
+                            <div className="team__logo"><img src={GobelinsLogo} alt="Logo Goeblins" /></div>
                         </div>
                         <div className="team__worker">
                             <div className="worker">
